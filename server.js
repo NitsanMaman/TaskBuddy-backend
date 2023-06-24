9c7218569ec20a7f803cf60171a89b51d6656ce9
Nitsan.Maman <nit.maman@gmail.com>
2023-06-24 22:38:13 +0300

    Ready to deploy backend 6

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const taskRoutes = require('./routes/tasks')
 const userRoutes = require('./routes/user')
+const cors = require('cors'); // Import the cors package
 const PORT = process.env.PORT || 4000
 
 // clear    To clear the terminal
